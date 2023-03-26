e1b1fb43f34e9d3ca0a8fe3c6175c5ad5ff987d2
seokjoon911 <116937147+seokjoon911@users.noreply.github.com>
2023-03-26 14:43:47 +0900

    Update 오븐 시계.java

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
-
+ 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+ 
         int A = sc.nextInt();
         int B = sc.nextInt();
         int C = sc.nextInt();
         
         sc.close();
-        
-        A += C / 60;
-        B += C % 60;
-        
-        if (B >= 60) {
-            A += 1;
-            B -= 60;
-        }
-        
-        if (A >= 24) {
-            A -= 24;
-        }
-        
-        System.out.printf(A + " " + B);
+ 
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+ 
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+ 
+        System.out.println(hour + " " + minute);
     }
 }
