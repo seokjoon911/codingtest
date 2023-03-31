1cb8c6aa531cb241d071b1a91bb82b447bc49020
seokjoon911 <116937147+seokjoon911@users.noreply.github.com>
2023-03-31 19:53:31 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 232 ms, Memory: 18320 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[26];
+        String S = sc.nextLine();
+        sc.close();
+        
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = -1;
+        }
+        
+        for(int i=0; i<S.length(); i++) {
+            char ch = S.charAt(i);
+            
+            if(arr[ch - 'a'] == -1) {
+                arr[ch - 'a'] = i;
+            }
+        }
+        
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        } 
+    }
+}
