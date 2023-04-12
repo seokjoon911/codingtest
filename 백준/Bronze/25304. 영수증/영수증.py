4e1280962dc3a56c3c6a11eb3eaca5cdd46179bb
Seokjoon Lee <116937147+seokjoon911@users.noreply.github.com>
2023-04-13 00:15:04 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+total= int(input())
+count= int(input())
+sum=0
+ 
+for i in range(count):
+    a, b = map(int,input().split())
+    sum += a*b
+    
+if total == sum : 
+    print("Yes")
+    
+else :
+    print("No")
