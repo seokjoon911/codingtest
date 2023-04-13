d7befc770b84e6fa32dfc7f8cfd7b9f652d3dff6
Seokjoon Lee <116937147+seokjoon911@users.noreply.github.com>
2023-04-14 00:32:19 +0900

    Update 체스판 다시 칠하기.py

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -5,23 +5,22 @@ cnt = []
 for i in range(N) :
     color.append(input())
     
-for a in range(N-7) :
-    for b in range(M-7) :
-        w_index = 0
-        b_index = 0
-        for i in range(a, a+8) :
-            for j in range(b, b+8) :
-                if (i+j) % 2 == 0 :
-                    if color[i][j] != 'W' :
-                        w_index += 1
-                    if color[i][j] != 'B' :
-                        b_index += 1
-                else :
-                    if color[i][j] != 'W' :
-                        b_index += 1
-                    if color[i][j] != 'B' :
-                        w_index += 1
-        cnt.append(w_index)
-        cnt.append(b_index)
-print(min(cnt))
-        
+for a in range(N-7) : 
+    for b in range(M-7) : #8*8로 자르기 위해, -7해준다
+        w_index = 0 #흰색으로 시작
+        b_index = 0 #검은색으로 시작
+        for i in range(a, a+8) : #시작지점
+            for j in range(b, b+8) : #시작지점
+                if (i+j) % 2 == 0 : #짝수인 경우
+                    if color[i][j] != 'W' : #W 아닐 경우
+                        w_index += 1 #W 칠하는 갯수
+                    if color[i][j] != 'B' : #B 아닐 경우
+                        b_index += 1 #B 칠하는 갯수
+                else : #홀수인 경우
+                    if color[i][j] != 'W' : #W 아닐 경우
+                        b_index += 1 #B 칠하는 갯수
+                    if color[i][j] != 'B' : #B 아닐 경우
+                        w_index += 1 #W 칠하는 갯수
+        cnt.append(w_index) #W로 시작할 때 경우의 수
+        cnt.append(b_index) #B로 시작할 때 경우의 수
+print(min(cnt))    
