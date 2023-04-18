9dea27041181a07c5ed385ad7562140fb9a66b08
Seokjoon Lee <116937147+seokjoon911@users.noreply.github.com>
2023-04-18 19:32:46 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,7 +1,4 @@
-numbers = []
-for _ in range(9):
-    i = int(input())
-    numbers.append(i)
-    
+numbers = [int(input()) for _ in range(9)]
+
 print(max(numbers))
-print(numbers.index(max(numbers))+1)
+print(numbers.index(max(numbers)) + 1)
