7550a57fc0f28cec15b28f9e440368bb7cae8524
Seokjoon Lee <116937147+seokjoon911@users.noreply.github.com>
2023-04-25 00:33:10 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = []
+    for num in s.split():
+        if num == "Z":
+            answer.pop()
+            continue
+        answer.append(int(num))
+        
+    return sum(answer)
