50ac4c7085509d5d9337f6781dfff2899ed80007
Seokjoon Lee <116937147+seokjoon911@users.noreply.github.com>
2023-04-28 21:39:59 +0900

    [Silver III] Title: 소수 구하기, Time: 5468 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,11 @@
+import sys
+m, n=map(int, sys.stdin.readline().split())
+
+for i in range(m,n+1) :
+    if i == 1 :
+        continue
+    for j in range(2, int(i**0.5)+1) :  # i의 약수를 구하기 위해 2부터 i의 제곱근까지 반복한다.
+        if i % j==0:           # 약수가 존재하므로 소수가 아님
+            break            # 더이상 검사할 필요가 없으므로 멈춤
+    else:
+        print(i)             # 소수인 경우 출력한다.
