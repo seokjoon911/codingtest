fac4b31235e08b90c46bd4498835668b7d3cdeb9
Seokjoon Lee <116937147+seokjoon911@users.noreply.github.com>
2023-05-18 16:45:14 +0900

    [unrated] Title: 배열 만들기 4, Time: 4.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,15 +1,15 @@
 def solution(arr):
-    stack = [] # 스택 초기화
+    stk = [] # 스택 초기화
     i = 0 # 인덱스 초기화
     
     while i < len(arr):
-        if not stack: # 스택이 비어있으면, 현재 배열의 값 추가
-            stack.append(arr[i])
+        if not stk: # 스택이 비어있으면, 현재 배열의 값 추가
+            stk.append(arr[i])
             i += 1
-        elif stack[-1] < arr[i]: # 현재 배열의 값이 스택의 마지막 값보다 크면, 스택에 추가
-            stack.append(arr[i])
+        elif stk[-1] < arr[i]: # 현재 배열의 값이 스택의 마지막 값보다 크면, 스택에 추가
+            stk.append(arr[i])
             i += 1
         else: # 현재 배열의 값이 스택의 마지막 값보다 작거나 같으면, 스택에서 마지막 값 삭제
-            stack.pop()
+            stk.pop()
     
-    return stack
+    return stk
